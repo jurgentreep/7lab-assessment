1947982984e17ccb3edac1ab3d317f22d52264d5
Jurgen Treep <jurgen.treep@gmail.com>
2016-09-04 20:30:21 +0000

    fix CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,6 +33,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Barryvdh\Cors\HandleCors::class,
             'throttle:60,1',
             'bindings',
         ],
@@ -54,5 +55,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
